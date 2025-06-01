d2b13a3411f6551ee79400914afeb26808254c1f
Murat Can Koç <muratcankoc@gmail.com>
2025-06-01 17:43:59 +0200

    Initial Commit

---
+++ NestZone/NestZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestZoneApp.swift
+//  NestZone
+//
+//  Created by muratcankoc on 01/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct NestZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
